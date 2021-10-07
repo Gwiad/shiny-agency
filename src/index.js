225964a55b87dcf3ff8082c05a9c6e2905dfa3e3
Gwiad <diorroer@gmail.com>
2021-10-07 15:36:28 +0200

    spa router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from 'pages/Home';
+import Survey from 'pages/Survey';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/survey" exact>
+        <Survey />
+      </Route>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Survey.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Survey() {
+  return (
+    <div>
+      <h1>Questionnaire 🧮</h1>
+    </div>
+  );
+}
