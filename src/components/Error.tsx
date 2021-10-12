95d05da29860128f3bab54183068ae857fdf5a69
Gwiad <diorroer@gmail.com>
2021-10-12 13:05:22 +0200

    404

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import colors from 'utils/styles/colors';
+import errorImg from 'assets/404.svg';
+
+const StyledContainer = styled.div`
+  background: ${colors.backgroundLight};
+  margin-left: 65px;
+  margin-right: 65px;
+  height: calc(100vh - 200px);
+  padding-top: 176px;
+  padding-left: 98px;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledImg = styled.img`
+  margin-left: 50%;
+  margin-right: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 function Error() {
   return (
-    <div>
-      <h1>Oups 🙈 Cette page nexiste pas</h1>
-    </div>
+    <StyledContainer>
+      <StyledImg src={errorImg} alt="404 erreur" />
+    </StyledContainer>
   );
 }
 
